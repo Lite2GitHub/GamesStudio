bd02c82c6b5d99ccfe7e8e3ed4b8fb073d35b1de
Lite2GitHub <ngdiyi2@gmail.com>
2024-04-15 13:25:34 +1000

    some coding
    need to make ui buttons :(

--- Assets/Scripts/NPCInteraction.cs
+++ Assets/Scripts/NPCInteraction.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NPCInteraction : MonoBehaviour
 {
-    public string reqItemName = "Key";
-    public GameObject dialogueCanvas; //Back story + Prompt
+    public GameObject dialogueCanvas; // Back story + Prompt
+    public GameObject inventoryMenu; // Menu UI 
     public bool playerInRange;
+    public PlayerController playerController;
+    public InventoryManager playerInventory; // inventory
+
+    public Text interactionText;
+
+    private string requiredItemIdentifier; // Not sure whether to use string or gameobject 
+    // private bool isInteracting = false; // redundancy?
 
     private void OnTriggerEnter(Collider other)
     {
@@ -28,19 +36,45 @@ public class NPCInteraction : MonoBehaviour
     {
         if (playerInRange && Input.GetKeyDown(KeyCode.Mouse1)) //right click to interact
         {
-            dialogueCanvas.SetActive(true); //open up prompt
-
-
-            if (InventoryManager.Instance.HasItem(reqItemName))
-            {
-                // Player has the required item, initiate dialogue for giving the item
-                
-            }
-            else
-            {
-                // Player does not have the required item, display a message or do nothing
-            }
+            Debug.Log("Interact by click work");
+            dialogueCanvas.SetActive(true); // open up prompt
+            inventoryMenu.SetActive(true); // open up inventory menu
+            playerController.SetInteracting(true); // disables player controller
+            
+            // isInteracting = true; // redundacy 
+            
         }
+    }
+
+    public void SelectItemFromInventory (string itemIdentifier)
+    {
+        if (itemIdentifier == requiredItemIdentifier && playerInventory.HasItem(requiredItemIdentifier))
+        {
+            playerInventory.RemoveItem(requiredItemIdentifier); // correct item taken/consumed
+            Debug.Log("Correct item selected & consumed");
+            interactionText.text = "spirit is relieved.";
+        } 
+        else 
+        { 
+            Debug.Log("Incorrect item selected or player does not have the item required.");
+            interactionText.text = "spirit is confused.";
+            // enabling the lockdown timer probably starts from here
+        }
+
+        CloseMenu();
+    }
+
+    void CloseMenu()
+    {
+        // isInteracting = false;
+        playerController.SetInteracting(false); // Enable player movement
+        inventoryMenu.SetActive(false);
+        interactionText.text = "";
+    }
+
+    public void SetRequiredItem(string itemIdentifier)
+    {
+        requiredItemIdentifier = itemIdentifier;
     }
 
 }
