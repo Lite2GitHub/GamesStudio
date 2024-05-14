d2bfdcd2a2b101dc38d71001811da4b48bf5dc8c
ShaunShepherd <shaun.shepherd142@gmail.com>
2024-05-15 08:26:23 +1000

    adding shader

---
+++ Assets/Scripts/BedInteraction.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BedInteraction : MonoBehaviour, IInteractable
+{
+    public void hover(bool hovering)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void interact(string context)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void LeftRange()
+    {
+        throw new System.NotImplementedException();
+    }
+}
