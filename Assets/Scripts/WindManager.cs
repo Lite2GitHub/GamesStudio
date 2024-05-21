03807d215de734678c8a3bc3486b6065a72dd017
a-clark <art.aidan.clark@gmail.com>
2024-05-21 16:55:21 +1000

    added canopy and wind script
    -wind script WIP, having some issues waking particles

---
+++ Assets/Scripts/WindManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindManager : MonoBehaviour
+{
+    public GameObject[] particles;
+    public GameObject[] locations;
+
+    float particleDuration = 10;
+    GameObject currentParticle;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //StartCoroutine(Wait(Random.Range(2, 6)));
+        instantiateParticle();
+    }
+
+    IEnumerator Wait(float waitTime)
+    {
+        yield return new WaitForSeconds(waitTime);
+        instantiateParticle();
+    }
+
+    void instantiateParticle()
+    {
+        int particle = Random.Range(0, particles.Length);
+        int location = Random.Range(0, locations.Length);
+
+        //only instantiate particle if there is not already one present
+        if (locations[location].transform.childCount < 1)
+        {
+            GameObject windParticle = Instantiate(particles[particle], locations[location].transform);
+            currentParticle = windParticle;
+        }
+        
+    }
+}
