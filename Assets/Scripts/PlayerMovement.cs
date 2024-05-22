811c42d7b2129ad7a1d19879308798ba6983c147
ShaunShepherd <shaun.shepherd142@gmail.com>
2024-05-23 00:27:30 +1000

    added walking anim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] SpriteRenderer playerSprite;
+    [SerializeField] Animator playerAnimator;
 
     [Header("Movement Variables")]
     [SerializeField] float movementSpeed;
@@ -27,6 +28,15 @@ public class PlayerMovement : MonoBehaviour
         movement.z = Input.GetAxisRaw("Vertical");
 
         CheckFlipSprite();
+
+        if (movement.x > 0.1 || movement.z > 0.1 || movement.x < -0.1 || movement.z < -0.1)
+        {
+            playerAnimator.SetBool("Walking", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("Walking", false);
+        }
     }
 
     void FixedUpdate()
